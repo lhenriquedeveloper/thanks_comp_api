6c7b987fa11327fde3b5ec33ef4c38847f098f17
lhenriquedev <sousarodriguesluishenrique@gmail.com>
2023-04-08 17:29:06 -0300

    uploading file changes

--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXnRoyu5uurtR7t-PfxLWNR5gfjtyZjEo",
-  authDomain: "thanks-comp-storage.firebaseapp.com",
-  projectId: "thanks-comp-storage",
-  storageBucket: "thanks-comp-storage.appspot.com",
-  messagingSenderId: "610240686728",
-  appId: "1:610240686728:web:13cfd69079074b4d59cd3d"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 
